cc5aade349c15a6494011dfb208c80452d1e73d5
shrirakshakr <shrikr27@gmail.com>
2025-07-19 12:31:10 +0530

    2nd

--- Assignment3/todo.js
+++ Assignment3/todo.js
@@ -1,14 +1,16 @@
-function Add(){
-    const display = document.getElementById("display");
-    let enter = document.getElementById("enter");
-    enter.value += `<li>${display.value}</li>`;
+// function Add(){
+//     const display = document.getElementById("display");
+//     let enter = document.getElementById("taskList");
+//     enter.value += `<li>${display.value}</li>`;
     
-}
-function cleardis(){
-    let enter = document.getElementById("enter");
-    enter.value = "";
-}
-// function getDatas(){
+// }
+// function cleardis(){
+//     let enter = document.getElementById("taskList");
+//     enter.value = "";
+// }
+
+
+
 //     setTimeout(() =>{
 //         let output="";
 //         datas.forEach((data,index) =>{
@@ -17,3 +19,30 @@ function cleardis(){
 //         document.body.innerHTML=output;
 //     },3000);
 // }
+
+
+function Add() {
+  const display = document.getElementById("display");
+  const taskList = document.getElementById("taskList");
+
+  if (display.value.trim() !== "") {
+    const li = document.createElement("li");
+    const btn=document.createElement("button");
+    btn.textContext="Delete";
+    btn.onclick=function(){
+        taskList.removeChild(li);
+    };
+    li.textContent = display.value;
+    li.appendChild(btn);
+    taskList.appendChild(li);
+    display.value = "";
+  }
+}
+
+function cleardis() {
+  const taskList = document.getElementById("taskList");
+  taskList.textContent = "";
+}
+
+
+
