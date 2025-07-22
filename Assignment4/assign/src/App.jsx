c5cd5b0dd36db5ed5fb33b6f1c21830c29a30c37
shrirakshakr <shrikr27@gmail.com>
2025-07-22 11:43:04 +0530

    assign4

--- Assignment4/assign/src/App.jsx
+++ Assignment4/assign/src/App.jsx
@@ -1,15 +1,13 @@
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Counter from "./counter"
 
 function App(){
 
   return (
     <div>
-      <Counter num="0" />
+      <Counter num={0} />
     </div>
   )
 }
 
-export default App
+export default App;
--- Assignment4/assign/src/counter.jsx
+++ Assignment4/assign/src/counter.jsx
@@ -4,13 +4,18 @@ function Counter(props)
         const[count, setCount]=useState(props.num)
         function setDec()
         {
-            
+            if (count > 0) {
+                setCount(count - 1);
+            }
+            else {
+                setCount(0);    
+            }
         }
         return(
             <div>
                 <h2>Counter : {count}</h2>
                 <button onClick={()=>setCount(count+1)}>Increment</button>
-                <button onClick={()=>setCount(count-1)>=0}>Decrement</button>
+                <button onClick={()=>setDec()}>Decrement</button>
                 <button onClick={()=>setCount(0)}>Reset</button>
             </div>
         )
