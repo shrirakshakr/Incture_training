97a0fa095c5d0a5b159c1bc97b4933d3bf7d037a
shrirakshakr <shrikr27@gmail.com>
2025-07-23 12:01:43 +0530

    assign5-palin

--- Assignment5/palinbyadd.js
+++ Assignment5/palinbyadd.js
@@ -1,32 +1,57 @@
-function shortestPalindrome(s)
-{
-    let right=s.length-1;
-    let char=new Set();
-    while(right>=0)
-    {
-        char.add(s[right]);
-        right--;
-    }
-    console.log(char);
-    let a=[];
-    for( let i of char)
-    {
-        a.push(i);
-    }
-    console.log(a);
-    for(let j=0;j<s.length;j++)
-    {
-        let last=s.length;
-        while(s[j]==s[last])
-        {
+function shortestPalindrome(s) {
+    function isPalindrome(s, first, last) {
+        while (first < last) {
+            if (s[first] !== s[last]) {
+                return false;
+            }
+            first++;
             last--;
-            j++;
         }
-        
+        return true;
     }
-    
+
+    let n = s.length;
+    let maxpalin = 0;
+    let result = "";
+
+    for (let i = n - 1; i >= 0; i--) {
+        if (isPalindrome(s, 0, i)) {
+            maxpalin = i;
+            break;
+        }
+    }
+
+    for (let i = n - 1; i > maxpalin; i--) {
+        result += s[i];
+    }
+
+    for (let a = 0; a < n; a++) {
+        result += s[a];
+    }
+
+    console.log(result);  // <-- this prints the final answer
 }
-shortestPalindrome("abc");
+
+shortestPalindrome("aacbcaaa");  // should print: aacabccaaa
+
+
+
+// let right=s.length-1;
+    // let char=new Set();
+    // while(right>=0)
+    // {
+    //     char.add(s[right]);
+    //     right--;
+    // }
+    // console.log(char);
+    // let a=[];
+    // for( let i of char)
+    // {
+    //     a.push(i);
+    // }
+    // console.log(a);
+
+
 // for(let i=1; i<=1000; i++)
 // {
     // let temp=i;
