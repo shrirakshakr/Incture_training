acdc9d2a80f5c498730274a63a1fef2408f82ab6
shrirakshakr <shrikr27@gmail.com>
2025-07-23 21:50:55 +0530

    assignment5

--- Assignment5/palinbyadd.js
+++ Assignment5/palinbyadd.js
@@ -1,4 +1,19 @@
 function shortestPalindrome(s) {
+    // let right=s.length-1;
+// let char=new Set();
+// while(right>=0)
+// {
+//     char.add(s[right]);
+//     right--;
+// }
+// console.log(char);
+// let a=[];
+// for( let i of char)
+// {
+//     a.push(i);
+// }
+// console.log(a);
+
     function isPalindrome(s, first, last) {
         while (first < last) {
             if (s[first] !== s[last]) {
@@ -9,10 +24,18 @@ function shortestPalindrome(s) {
         }
         return true;
     }
-
+    //{
+// let b=s.rev();
+// console.log(b);
+// for(let i=0; i<s.length; i++)
+// {
+//     let temp=i;
+//     let rev=0;
+// }
+//}
     let n = s.length;
     let maxpalin = 0;
-    let result = "";
+    
 
     for (let i = n - 1; i >= 0; i--) {
         if (isPalindrome(s, 0, i)) {
@@ -20,6 +43,7 @@ function shortestPalindrome(s) {
             break;
         }
     }
+    let result = "";
 
     for (let i = n - 1; i > maxpalin; i--) {
         result += s[i];
@@ -29,33 +53,20 @@ function shortestPalindrome(s) {
         result += s[a];
     }
 
-    console.log(result);  // <-- this prints the final answer
+    console.log(result); 
 }
 
-shortestPalindrome("aacbcaaa");  // should print: aacabccaaa
+shortestPalindrome("abcbba");  
 
 
 
-// let right=s.length-1;
-    // let char=new Set();
-    // while(right>=0)
-    // {
-    //     char.add(s[right]);
-    //     right--;
-    // }
-    // console.log(char);
-    // let a=[];
-    // for( let i of char)
-    // {
-    //     a.push(i);
-    // }
-    // console.log(a);
+
 
 
 // for(let i=1; i<=1000; i++)
 // {
-    // let temp=i;
-    // let rev=0;
+// let temp=i;
+// let rev=0;
 //     while(temp>0)
 //     {
 //         let rem=temp%10;
@@ -68,12 +79,3 @@ shortestPalindrome("aacbcaaa");  // should print: aacabccaaa
 //     }
 // }
 
-//{
-// let b=s.rev();
-    // console.log(b);
-    // for(let i=0; i<s.length; i++)
-    // {
-    //     let temp=i;
-    //     let rev=0;
-    // }
-//}
