cb173f764ad1734a71dfee6dee2a5a5bc76143fb
shrirakshakr <shrikr27@gmail.com>
2025-07-27 22:43:28 +0530

    asssign8

--- Assignment8/coin.js
+++ Assignment8/coin.js
@@ -1,34 +1,47 @@
-function minCoinsToMakeAmount(coins, amount)
-{
-    coins=[5,10];
-    for(let i=0;i<coins.length-1;i++)
-    {
-        for(let j=0;j<coins.length-i-1;j++)
-        {
-            if(coins[j]>coins[j+1])
-            {
-                let temp=coins[j];
-                coins[j]=coins[j+1];
-                coins[j+1]=temp;
+function minCoinsToMakeAmount(coins, targetAmount) {
+    // Sort the coins in ascending order using bubble sort
+    for (let i = 0; i < coins.length - 1; i++) {
+        for (let j = 0; j < coins.length - i - 1; j++) {
+            if (coins[j] > coins[j + 1]) {
+                let temp = coins[j];
+                coins[j] = coins[j + 1];
+                coins[j + 1] = temp;
             }
         }
     }
-    let sum=0;
-    let count=0;
-    for(let i=coins.length-1;i>=0;i--)
-    {
-        while(sum+coins[i]<=amount)
-        {
-            sum+=coins[i];
-            count++;
+
+    // Create an array to store the minimum number of coins needed for every amount from 0 to targetAmount
+    let minCoins = new Array(targetAmount + 1).fill(Infinity);
+
+    // To make amount 0, we need 0 coins
+    minCoins[0] = 0;
+
+    // Loop through all amounts from 1 to targetAmount
+    for (let currentAmount = 1; currentAmount <= targetAmount; currentAmount++) {
+        // Try using each coin to make the current amount
+        for (let c = 0; c < coins.length; c++) {
+            let coinValue = coins[c];
+
+            // If the coin is smaller than or equal to currentAmount,
+            // check if we can use it to get fewer coins
+            if (coinValue <= currentAmount) {
+                let remainingAmount = currentAmount - coinValue;
+                let coinsNeeded = minCoins[remainingAmount] + 1;
+
+                if (coinsNeeded < minCoins[currentAmount]) {
+                    minCoins[currentAmount] = coinsNeeded;
+                }
+            }
         }
     }
-    if(sum===amount)
-    {
-        console.log(count);
-    }
-    else{
+    if (minCoins[targetAmount] === Infinity) {
         console.log("-1");
+    } else {
+        console.log(minCoins[targetAmount]);
     }
 }
-minCoinsToMakeAmount(34);
+
+
+minCoinsToMakeAmount([1, 2, 5], 6);     
+
+
